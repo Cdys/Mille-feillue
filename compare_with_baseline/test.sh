45e45027abd4a401befd3b17e50d995a4d99578a
kk1234a2 <ydc6667@163.com>
2024-04-15 18:57:13 +0800

    first commit

--- compare_with_baseline/test.sh
+++ compare_with_baseline/test.sh
@@ -1,10 +1,10 @@
-input="cg_matrix_set.csv" #The name of dataset
+input="CG_dataset" #The name of dataset
 {
   read
   i=1
   while IFS=',' read -r name nnz
   do
-    for matrix in `find "/home/weifeng/MM/" -name "$name.mtx"` #The road of data
+    for matrix in `find "/home/dataset/MM/" -name "$name.mtx"` #The road of data
     do
         ./Mille-feuille_CG_NVIDIA $matrix
         ./Mille-feuille_BiCGSTAB_NVIDIA $matrix
