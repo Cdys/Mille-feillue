d0c76a1236bb4e936a9e768e692e2ac14ddd890f
kk1234a2 <ydc6667@163.com>
2024-04-15 17:06:41 +0800

    first commit

--- compare_with_baseline/test.sh
+++ compare_with_baseline/test.sh
@@ -1,22 +1,15 @@
-input="cg_matrix_set.csv"
+input="cg_matrix_set.csv" #The name of dataset
 {
   read
   i=1
-  while IFS=',' read -r data a b c d
+  while IFS=',' read -r name nnz
   do
-    for data1 in `find "/home/weifeng/MM/" -name "$data.mtx"`
+    for matrix in `find "/home/weifeng/MM/" -name "$name.mtx"` #The road of data
     do
-        echo -n $data1
-        #timeout -s 9 1m ./bykrylov_bicg_syncfree $data1 16
-        #timeout -s 9 1m ./bykrylov_bicg_syncfree_resort $data1 $each_nnz
-        timeout -s 9 1m ./bykrylov_initial $data1
-        #timeout -s 9 1m ./bykrylov_bicg_syncfree_resort_mixed_precision $data1 $each_nnz
-        # timeout -s 9 1m ./bykrylov_cg_syncfree_resort_mixed_precision $data1 32
-        # timeout -s 9 1m ./bykrylov_cg_syncfree_resort_mixed_precision $data1 64
-        # timeout -s 9 1m ./bykrylov_cg_syncfree_resort_mixed_precision $data1 128
-        # timeout -s 9 1m ./bykrylov_cg_syncfree_resort_mixed_precision $data1 256
-        # timeout -s 9 1m ./bykrylov_cg_syncfree_resort_mixed_precision $data1 512
-        # timeout -s 9 1m ./bykrylov_cg_syncfree_resort_mixed_precision $data1 1024
+        ./Mille-feuille_CG $matrix
+        ./Mille-feuille_BiCGSTAB $matrix
+        ./cuSPARSE_CG $matrix
+        ./cuSPARSE_BiCGSTAB $matrix
     done
     i=`expr $i + 1`
   done 
